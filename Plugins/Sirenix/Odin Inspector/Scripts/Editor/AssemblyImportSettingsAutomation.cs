d4512f7ad037fa143d83528090e203af6fce22d9
Ikimasho_Cherif <cherif.younis@ikimasho.games>
2022-06-01 16:34:23 +0200

    1.5.0 : Removed Odin demo folder

--- Plugins/Sirenix/Odin Inspector/Scripts/Editor/AssemblyImportSettingsAutomation.cs
+++ Plugins/Sirenix/Odin Inspector/Scripts/Editor/AssemblyImportSettingsAutomation.cs
@@ -39,7 +39,7 @@ namespace Sirenix.OdinInspector.Editor
             var assemblyDir = new DirectoryInfo(SirenixAssetPaths.SirenixAssembliesPath).FullName;
             var projectAssetsPath = Directory.GetCurrentDirectory().TrimEnd('\\', '/');
 
-            var isPackage = true; //PathUtilities.HasSubDirectory(new DirectoryInfo(projectAssetsPath), new DirectoryInfo(assemblyDir)) == false;
+            var isPackage = PathUtilities.HasSubDirectory(new DirectoryInfo(projectAssetsPath), new DirectoryInfo(assemblyDir)) == false;
 
             var aotDirPath = assemblyDir + "NoEmitAndNoEditor/";
             var jitDirPath = assemblyDir + "NoEditor/";
